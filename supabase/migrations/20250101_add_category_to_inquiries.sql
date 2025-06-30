6b2d1c40ebf6e56ec06cc84e92b51a95304db7e1
twoimo <twoimo@dgu.ac.kr>
2025-06-30 20:23:47 +0900

    Remove unused image URLs and category column migration from inquiries table

--- supabase/migrations/20250101_add_category_to_inquiries.sql
+++
@@ -1,8 +0,0 @@
--- Add category column to inquiries table
-ALTER TABLE public.inquiries 
-ADD COLUMN category text DEFAULT '일반문의' NOT NULL;
-
--- Update existing records to have a default category
-UPDATE public.inquiries 
-SET category = '일반문의' 
-WHERE category IS NULL; 
